274627aa9576b6abd9892206765a0399afc1e146
Haifa <haifanaj@gmail.com>
2021-02-23 17:52:39 -0600

    convert string to intger

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // set the dimentions and the margins of the graph
 var margin = {top:10, right:30, bottom:30 , left:30};
-var svgWidth = 900;
+var svgWidth = 800;
 var svgHeight = 600;
 
 var chartWidth= svgWidth-margin.left-margin.right;
@@ -18,10 +18,13 @@ var svg= d3.select("#scatter")
 // read the data path based to index file 
 d3.csv("assets/data/data.csv").then(function(journalData){
     console.log("data",journalData)
+    
+    console.log(d3.max(journalData, d => +(d.poverty)))
+
     // add x axis
     var xScale=d3.scaleLinear()
-            .domain([0,d3.max(journalData, d =>d.poverty)])
-            .range([0,chartWidth]);
+            .domain([0,d3.max(journalData, d =>+(d.poverty))])
+            .range([0,chartWidth])
         svg.append("g")
             // x axis to bottom
             .attr("transform",`translate(0,${chartHeight})`)
@@ -29,10 +32,20 @@ d3.csv("assets/data/data.csv").then(function(journalData){
             .call(d3.axisBottom(xScale));
     // add y axis
     var yScale=d3.scaleLinear()
-            .domain([0,d3.max(journalData,d => d.healthcare)])
+            .domain([0,d3.max(journalData,d => +(d.healthcare))])
             .range([chartHeight,0])
         svg.append("g")
-            .call(d3.axisLeft(yScale))
+            .call(d3.axisLeft(yScale));
+
+        svg.append("g").selectAll(".stateCircle")
+        .data(journalData)
+        .enter()
+        .append("circle")
+        .classed("stateCircle", true)
+            .attr("cx",d => +(d.poverty))
+            .attr("cy",d => +(d.healthcare))
+            .attr("r",10)
+
 
     
 
